72bd2d4361cb42f8a324491939034f663ce429f7
Gabriel Caroni <gabrielcaroni@hotmail.com>
2011-04-30 19:33:52 -0400

    Controlador Proyecto

---
+++ saip/controllers/proyecto_controller.py
@@ -0,0 +1,22 @@
+from tgext.crud import CrudRestController
+from saip.model import DBSession, Proyecto
+from sprox.tablebase import TableBase #para manejar datos de prueba
+from sprox.fillerbase import TableFiller #""
+
+class ProyectoTable(TableBase): #para manejar datos de prueba
+	__model__ = Proyecto
+	__omit_fields__ = ['id']
+proyecto_table = ProyectoTable(DBSession)
+
+class ProyectoTableFiller(TableFiller):#para manejar datos de prueba
+	__model__ = Proyecto
+proyecto_table_filler = ProyectoTableFiller(DBSession)
+
+class ProyectoController(CrudRestController):
+	model = Proyecto
+	table = proyecto_table
+	table_filler = proyecto_table_filler
+
+#class RootController(BaseController):
+#	proyectos = ProyectoController(DBSession)
+
