f603023470d594615c00771bb54ffa4083ba5646
Rodrigo Parra <rodpar07@gmail.com>
2011-05-04 12:12:13 -0400

    modelo

--- saip/model/app.py
+++ saip/model/app.py
@@ -4,7 +4,7 @@
 from sqlalchemy import *
 from sqlalchemy.orm import mapper, relation
 from sqlalchemy import Table, ForeignKey, Column
-from sqlalchemy.types import Integer, String
+from sqlalchemy.types import Integer, Unicode
 from sqlalchemy.orm import relation, backref
 
 from saip.model import DeclarativeBase, metadata, DBSession
@@ -14,25 +14,15 @@ class Proyecto(DeclarativeBase):
     mapeada a la tabla proyectos de forma declarativa. """ 
 
     __tablename__ = 'proyectos'
-    id = Column(String, primary_key = True)
-    nombre = Column(String, nullable = False)
+    id = Column(Unicode, primary_key = True)
+    nombre = Column(Unicode, nullable = False)
     fecha_inicio = Column(Date)
     fecha_fin = Column(Date)
-    descripcion = Column(String)
-    estado = Column(String, nullable = False)
+    descripcion = Column(Unicode)
+    estado = Column(Unicode, nullable = False)
     nro_fases = Column(Integer, nullable = False)
     
-    def __init__(self, id, nombre, fecha_inicio, fecha_fin, estado, nro_fases, descripcion = ""):
-        """ Constructor de la clase proyecto."""
-
-        self.id = id
-        self.nombre = nombre
-        self.fecha_inicio = fecha_inicio
-        self.fecha_fin = fecha_fin
-        self.descripcion = descripcion
-        self.estado = estado
-        self.nro_fases = nro_fases
-
+    
     def agregar_fase(self, fase):
         """ Permite agregar una fase al proyecto dado. """
         
@@ -44,29 +34,18 @@ class Fase(DeclarativeBase):
     mapeada a la tabla fases de forma declarativa. """ 
 
     __tablename__ = 'fases'
-    id = Column(String, primary_key = True)
-    nombre = Column(String, nullable = False)
+    id = Column(Unicode, primary_key = True)
+    nombre = Column(Unicode, nullable = False)
     orden = Column(Integer, nullable = False)
     fecha_inicio = Column(Date)
     fecha_fin = Column(Date)
-    descripcion = Column(String)
-    estado = Column(String, nullable = False)
-    id_proyecto = Column(String, ForeignKey("proyectos.id"))
+    descripcion = Column(Unicode)
+    estado = Column(Unicode, nullable = False)
+    id_proyecto = Column(Unicode, ForeignKey("proyectos.id"))
     
     proyecto = relation("Proyecto", backref = backref('fases', order_by=id))
 
-    def __init__(self, id, nombre, orden, fecha_inicio, fecha_fin, estado, proyecto = "", descripcion = ""):
-        """ Constructor de la clase fase."""
-
-        self.id = id
-        self.nombre = nombre
-        self.orden = orden
-        self.fecha_inicio = fecha_inicio
-        self.fecha_fin = fecha_fin
-        self.descripcion = descripcion
-        self.estado = estado
-        self.proyecto = proyecto
-
+    
     def agregar_tipo_item(self, tipo_item):
         """ Permite agregar un tipo de item a la fase dada. """
         
@@ -88,20 +67,12 @@ class TipoItem(DeclarativeBase):
     mapeada a la tabla tipos_item de forma declarativa. """ 
 
     __tablename__ = 'tipos_item'
-    id = Column(String, primary_key = True)
-    nombre = Column(String, nullable = False)
-    descripcion = Column(String)
-    id_fase = Column(String, ForeignKey("fases.id"))
+    id = Column(Unicode, primary_key = True)
+    nombre = Column(Unicode, nullable = False)
+    descripcion = Column(Unicode)
+    id_fase = Column(Unicode, ForeignKey("fases.id"))
     
     fase = relation("Fase", backref = backref('tipos_item', order_by=id))
-
-    def __init__(self, id, nombre, fase = "", descripcion = ""):
-        """ Constructor de la clase tipo de item."""
-
-        self.id = id
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.fase = fase
 
     def agregar_caracteristica(self, caracteristica):
         """ Permite agregar una carateristica al tipo de item dado. """
@@ -114,22 +85,15 @@ class Caracteristica(DeclarativeBase):
     mapeada a la tabla caracteristicas de forma declarativa. """ 
 
     __tablename__ = 'caracteristicas'
-    id = Column(String, primary_key = True)
-    nombre = Column(String, nullable = False)
-    tipo = Column(String)
-    descripcion = Column(String)    
-    id_tipo_item = Column(String, ForeignKey("tipos_item.id"))
+    id = Column(Unicode, primary_key = True)
+    nombre = Column(Unicode, nullable = False)
+    tipo = Column(Unicode)
+    descripcion = Column(Unicode)    
+    id_tipo_item = Column(Unicode, ForeignKey("tipos_item.id"))
     
     tipo_item = relation("TipoItem", backref = backref('caracteristicas', order_by=id))
 
-    def __init__(self, id, nombre, tipo, tipo_item = "", descripcion = ""):
-        """ Constructor de la clase caracteristica."""
-
-        self.id = id
-        self.nombre = nombre
-        self.tipo = tipo
-        self.tipo_item = tipo_item
-        self.descripcion = descripcion
+    
 
 
 class LineaBase(DeclarativeBase):
@@ -137,21 +101,12 @@ class LineaBase(DeclarativeBase):
     mapeada a la tabla lineas_base de forma declarativa. """ 
 
     __tablename__ = 'lineas_base'
-    id = Column(String, primary_key = True)
-    descripcion = Column(String)
-    estado = Column(String, nullable = False)
-    id_fase = Column(String, ForeignKey("fases.id"))
+    id = Column(Unicode, primary_key = True)
+    descripcion = Column(Unicode)
+    estado = Column(Unicode, nullable = False)
+    id_fase = Column(Unicode, ForeignKey("fases.id"))
     
     fase = relation("Fase", backref = backref('lineas_base', order_by=id))
-
-    def __init__(self, id, nombre, estado, fase = "", descripcion = ""):
-        """ Constructor de la clase linea base."""
-
-        self.id = id
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.estado = estado
-        self.fase = fase
 
 
     def agregar_item(self, item):
@@ -165,32 +120,21 @@ class Item(DeclarativeBase):
     mapeada a la tabla items de forma declarativa. """ 
 
     __tablename__ = 'items'
-    id = Column(String, primary_key = True)
-    nombre = Column(String, nullable = False)
-    descripcion = Column(String)
-    estado = Column(String, nullable = False)
-    anexo = Column(String)
-    observaciones = Column(String)
+    id = Column(Unicode, primary_key = True)
+    nombre = Column(Unicode, nullable = False)
+    descripcion = Column(Unicode)
+    estado = Column(Unicode, nullable = False)
+    anexo = Column(Unicode)
+    observaciones = Column(Unicode)
     prioridad = Column(Integer, nullable = False)
     complejidad = Column(Integer, nullable = False)
-    id_tipo_item = Column(String, ForeignKey("tipos_item.id"))
-    id_linea_base = Column(String, ForeignKey("lineas_base.id"))    
+    id_tipo_item = Column(Unicode, ForeignKey("tipos_item.id"))
+    id_linea_base = Column(Unicode, ForeignKey("lineas_base.id"))    
 
     tipo_item = relation("TipoItem", backref = backref('items', order_by=id))
     linea_base = relation("LineaBase", backref = backref('items', order_by=id))    
    
-    def __init__(self, id, nombre, estado, prioridad, complejidad, descripcion = "", anexo = "", observaciones = ""):
-        """ Constructor de la clase item."""
-
-        self.id = id
-        self.nombre = nombre
-        self.prioridad = prioridad
-        self.complejidad = complejidad
-        self.estado = estado
-        self.descripcion = descripcion
-        self.anexo = anexo
-        self.observaciones = observaciones
-
+    
     def agregar_archivo(self, archivo):
         """ Permite agregar un archivo al item dado. """
         
@@ -207,20 +151,14 @@ class Archivo(DeclarativeBase):
     mapeada a la tabla archivos de forma declarativa. """ 
 
     __tablename__ = 'archivos'
-    id = Column(String, primary_key = True)
-    nombre = Column(String)
+    id = Column(Unicode, primary_key = True)
+    nombre = Column(Unicode)
     contenido = Column(LargeBinary)    
-    id_item = Column(String, ForeignKey("items.id"))
+    id_item = Column(Unicode, ForeignKey("items.id"))
     
     item = relation("Item", backref = backref('archivos', order_by=id))
 
-    def __init__(self, id, nombre, item = "", contenido = ""):
-        """ Constructor de la clase archivo."""
-
-        self.id = id
-        self.nombre = nombre
-        self.item = item
-        self.contenido = contenido        
+        
 
 
 class Relacion(DeclarativeBase):
@@ -228,9 +166,9 @@ class Relacion(DeclarativeBase):
     mapeada a la tabla relaciones de forma declarativa. """ 
 
     __tablename__ = 'relaciones'
-    id = Column(String, primary_key = True)
-    id_item_1 = Column(String, ForeignKey("items.id"))
-    id_item_2 = Column(String, ForeignKey("items.id"))    
+    id = Column(Unicode, primary_key = True)
+    id_item_1 = Column(Unicode, ForeignKey("items.id"))
+    id_item_2 = Column(Unicode, ForeignKey("items.id"))    
     
     item_1 = relation("Item", primaryjoin=(id_item_1==Item.id))
     item_2 = relation("Item", primaryjoin=(id_item_2==Item.id))
@@ -248,15 +186,8 @@ class Revision(DeclarativeBase):
     mapeada a la tabla revisiones de forma declarativa. """ 
 
     __tablename__ = 'revisiones'
-    id = Column(String, primary_key = True)
-    descripcion = Column(String)    
-    id_item = Column(String, ForeignKey("items.id"))
+    id = Column(Unicode, primary_key = True)
+    descripcion = Column(Unicode)    
+    id_item = Column(Unicode, ForeignKey("items.id"))
     
     item = relation("Item", backref = backref('revisiones', order_by=id))
-
-    def __init__(self, id, item = "", descripcion = ""):
-        """ Constructor de la clase revision."""
-
-        self.id = id
-        self.item = item
-        self.descripcion = descripcion        
