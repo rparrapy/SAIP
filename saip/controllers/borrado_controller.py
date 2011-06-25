1e9ae195b9041d0f80cb130b175afab020f1b4bf
Gabriel Caroni <gabrielcaroni@hotmail.com>
2011-06-24 20:39:48 -0400

    borrado controller

--- saip/controllers/borrado_controller.py
+++ saip/controllers/borrado_controller.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+"""
+Módulo que define el controlador de ítems borrados.
+
+@authors:
+    - U{Alejandro Arce<mailto:alearce07@gmail.com>}
+    - U{Gabriel Caroni<mailto:gabrielcaroni@gmail.com>}
+    - U{Rodrigo Parra<mailto:rodpar07@gmail.com>}
+"""
 from tgext.crud import CrudRestController
 from saip.model import DBSession, Item, TipoItem, Caracteristica, Relacion
 from sprox.tablebase import TableBase
 from sprox.fillerbase import TableFiller
 from sprox.formbase import AddRecordForm
-from tg import tmpl_context #templates
+from tg import tmpl_context
 from tg import expose, require, request, redirect
 from tg.decorators import with_trailing_slash, paginate, without_trailing_slash
 from tgext.crud.decorators import registered_validate, catch_errors 
@@ -36,6 +44,9 @@ class ItemTable(TableBase):
 item_table = ItemTable(DBSession)
 
 class ItemTableFiller(TableFiller):
+    """ Clase que se utiliza para llenar las tablas de ítems borrados a ser
+        potencialmente recuperados.
+    """
     __model__ = Item
     buscado = ""
     id_fase = ""
@@ -44,6 +55,8 @@ class ItemTableFiller(TableFiller):
         return obj.tipo_item.nombre
 
     def __actions__(self, obj):
+        """ Define las acciones posibles para cada ítem borrado.
+        """
         primary_fields = self.__provider__.get_primary_fields(self.__entity__)
         pklist = '/'.join(map(lambda x: str(getattr(obj, x)), primary_fields))
         id_item = pklist.split("/")[0] 
@@ -75,6 +88,9 @@ class ItemTableFiller(TableFiller):
         self.id_fase = id_fase
     def _do_get_provider_count_and_objs(self, buscado = "", \
                                         id_fase = "", **kw):
+        """ Se utiliza para listar los ítems borrados que cumplan ciertas
+            condiciones y ciertos permisos.
+        """
         items = DBSession.query(Item).filter(Item \
                 .id_tipo_item.contains(self.id_fase)).filter(Item.borrado == \
                 True).all()
@@ -98,6 +114,8 @@ item_table_filler = ItemTableFiller(DBSession)
 
 
 class BorradoController(CrudRestController):
+    """ Controlador del modelo Item para los que se encuentran borrados.
+    """
     model = Item
     table = item_table
     table_filler = item_table_filler
@@ -116,6 +134,9 @@ class BorradoController(CrudRestController):
     @without_trailing_slash
     @expose()
     def revivir(self, *args, **kw):
+        """Permite realizar la recuperación de ítems borrados con sus
+            respectivos atributos y las relaciones que sea posible recuperar.
+        """
         if TienePermiso("recuperar item", id_fase = self.id_fase):        
             id_item = kw["id_item"]
             it = DBSession.query(Item).filter(Item.id == id_item).one()
@@ -151,6 +172,9 @@ class BorradoController(CrudRestController):
     @expose('json')
     @paginate('value_list', items_per_page=7)
     def get_all(self, *args, **kw):
+        """Lista los ítems borrados de acuerdo a lo establecido en
+           L{borrado_controller.ItemTableFiller._do_get_provider_count_and_objs}.
+        """
         item_table_filler.init("",self.id_fase)      
         d = super(BorradoController, self).get_all(*args, **kw)
         d["accion"] = "./buscar"
@@ -164,6 +188,9 @@ class BorradoController(CrudRestController):
     @expose('json')
     @paginate('value_list', items_per_page = 7)
     def buscar(self, **kw):
+        """ Lista los ítems borrados de acuerdo a un criterio de búsqueda
+            introducido por el usuario.
+        """
         buscar_table_filler = ItemTableFiller(DBSession)
         if "parametro" in kw:
             buscar_table_filler.init(kw["parametro"], self.id_fase)
@@ -179,6 +206,9 @@ class BorradoController(CrudRestController):
     @expose('saip.templates.get_all_caracteristicas_item')
     @paginate('value_list', items_per_page=7)
     def listar_caracteristicas(self, **kw):
+        """ Permite al usuario visualizar las características de un ítem
+            borrado.
+        """
         id_fase = unicode(request.url.split("/")[-4])
         if TieneAlgunPermiso(tipo = u"Fase", recurso = u"Item", id_fase = \
                             id_fase).is_met(request.environ):
